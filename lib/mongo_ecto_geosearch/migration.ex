f5bf8f08cae73d1498b0ea78d70682ac8277877c
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-11-03 10:24:49 +0100

    Fix migrations

--- lib/mongo_ecto_geosearch/migration.ex
+++ lib/mongo_ecto_geosearch/migration.ex
@@ -1,11 +1,31 @@
 defmodule Mongo.Ecto.GeoSearch.Migration do
   use Ecto.Migration
 
-  def create_2dsphere_index(relation, field) do
+  def create_2dsphere_index(relation, field, opts \\ %{})
+  def create_2dsphere_index(relation, field, opts) when is_atom(field) do
+    do_create_index(relation, field, opts)
+  end
+  def create_2dsphere_index(relation, field, opts) do
+    atom_field = String.to_atom(field)
+    do_create_index(relation, atom_field, opts)
+  end
+
+  defp do_create_index(relation, field, opts) do
     execute [
       createIndexes: relation,
-      key: {field, "2dsphere"} ,
-      "2dsphereIndexVersion": 3
+      indexes: [
+        [
+          key: [{field, "2dsphere"}],
+          name: name(field, opts),
+          "2dsphereIndexVersion": index_version(opts)
+        ]
+      ]
     ]
   end
+
+  defp name(field, opts) do
+    opts[:name] || "#{field}_2dsphere"
+  end
+
+  defp index_version(opts), do: opts[:index_version] || 3
 end
